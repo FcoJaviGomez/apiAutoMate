3db629c02d2469cc98deb0444a8e496dffe8ea09
FcoJavierGómez <javiergomez742@gmail.com>
2022-04-17 16:26:40 +0200

    prueba

--- apiAutoMate.js
+++ apiAutoMate.js
@@ -138,14 +138,6 @@ app.put("/usuario",
         console.log(request.body);
 
 
-        for(let atributo in request.body){
-
-            if(request.body[atributo]===''){
-                request.body[atributo]===null
-            }
-        }
-
-        console.log(request.body)
 
         let params = [request.body.name,
         request.body.last_name,
@@ -181,38 +173,6 @@ app.put("/usuario",
     }
 );
 
-app.put("/usuario/password",
-    function (request, response) {
-
-
-        let respuesta= {mensaje: true}
-        let sql = `SELECT * FROM user WHERE id_user=${request.body.id_user} AND password= ${request.body.contrasena}`;
-
-        connection.query(sql, function (err, result) {
-            if (err)
-                console.log(err);
-            else {
-                if(result.length===0){
-                    respuesta.mensaje = false
-                    response.send(respuesta)
-                }
-                else{
-                    let sql = `UPDATE user SET password = ${request.body.contrasenaNueva} WHERE id_user=${request.body.id_user}`
-
-                    connection.query(sql, function (err, result) {
-                        if (err)
-                            console.log(err);
-                        else {
-                            respuesta.mensaje = true
-                            response.send(respuesta)
-                        }
-                    })
-                    
-                }
-            }
-        })
-    }
-);
 
 app.get("/gastos",
     function (request, response) {
